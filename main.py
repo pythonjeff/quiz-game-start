3094081c8247650f69d5cb970066827d4f272977
Jeff Larson <jeffreyblarson00@gmail.com>
2024-02-22 19:56:15 -0600

    Changes to quiz game 2.20.24

---
+++ main.py
@@ -0,0 +1,13 @@
+from question_model import Question
+from data import question_data  
+
+question_bank = []
+
+
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    question_bank.append(Question(question_text, question_answer))
+
+
+print(question_bank)
